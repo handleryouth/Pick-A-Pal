342afa08a8b055fe9a98f30d2339480c4a33c7ab
Tony David <70279376+handleryouth@users.noreply.github.com>
2024-08-13 10:41:37 +0700

    Initial Commit

---
+++ Pick-A-Pal/Pick_A_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_A_PalApp.swift
+//  Pick-A-Pal
+//
+//  Created by Tony Gultom on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_A_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
